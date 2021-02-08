f52792e0cb7bf731218ace54d3f19e40c7c35d7b
Arjun V Pankajakshan <2038@tkmce.ac.in>
2021-02-08 19:06:20 +0530

    first Python Commit

---
+++ Python Programming/2leapyearinrange.py
@@ -0,0 +1,6 @@
+start=int(input("Enter the Starting Year"))
+end=int(input("Enter the Ending Year"))
+print("The leap years from "+str(start)+" to "+str(end)+" are ")
+for i in range(start,end+1):
+    if i%4==0:
+        print(i)
