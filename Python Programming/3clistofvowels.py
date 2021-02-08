30d011ad3ab0cc2774eaf6f919322ef3dcf3b3d7
Arjun V Pankajakshan <2038@tkmce.ac.in>
2021-02-09 00:08:56 +0530

    3c finished

--- Python Programming/3clistofvowels.py
+++ Python Programming/3clistofvowels.py
@@ -1,6 +1,6 @@
 vow=[]
-string=input("Enter the String")
-for i in range(str.len(string)):
-    if (string[i]=="a","e","i","o","u"):
+string=str(input("Enter the String"))
+for i in range(len(string)):
+    if (string[i]=="a" or string[i]=="e" or string[i]=="i" or string[i]=="o" or string[i]=="u"):
         vow.append(string[i])
-        print(vow)
+print(vow)
