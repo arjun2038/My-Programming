0ef4f1011762cd80d1ebf9f213f8ff42ff96df1e
ARJUN V PANKAJAKSHAN <2038@tkmce.ac.in>
2021-02-20 17:57:47 +0530

    Course Outcome 3 Completed

---
+++ Python Programming/Course Outcome3/Graphics/Graphics3D/cuboid.py
@@ -0,0 +1,4 @@
+def areacu(a,b,h):
+    result=2*((a*b)+(b*h)+(h*a))
+def perimetecu(a,b,h):
+    result=(a+b+h)*4
---
+++ Python Programming/Course Outcome3/Graphics/Graphics3D/sphere.py
@@ -0,0 +1,4 @@
+def areasp(r):
+    result=4*3.14*r*r
+def perimetesp(r):
+    result=(2*3.14*r)
